98a2abd1e51d0b26e3dca3cd138f0ac222ab261a
An-Tonic <aserdyukov@uclan.ac.uk>
2022-11-09 20:57:26 +0200

    function that loads the container and boxes 2 TODOs: input validation and print container and boxes

---
+++ Algorithms and Data Structures/Lab.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void main1() {
+
+	string ab = "#";
+
+	cout << ab;
+
+}
--- Algorithms and Data Structures/Source.cpp
+++ Algorithms and Data Structures/Source.cpp
@@ -6,15 +6,6 @@
 
 using namespace std;
 
-
-typedef struct PackerProblem {
-	unsigned short numberOfBoxes;
-	unsigned char width;
-	unsigned char length;
-	box boxes[];
-
-};
-
 typedef struct box {
 	unsigned char width;
 	unsigned char length;
@@ -22,28 +13,93 @@ typedef struct box {
 
 };
 
+typedef struct PackerProblem {
+	unsigned short numberOfBoxes;
+	unsigned char width;
+	unsigned char length;
+	
+	box boxes[30];
+
+};
+
+
 PackerProblem* loadPackerProblem(string filename) {
+	PackerProblem container;
 	ifstream file(filename);
+	string newLine;
+	unsigned short counter = 0;
+	unsigned short areaContainer = 0;
+	unsigned short sumAreaBoxes = 0;
+
+	//Reading and proof-checking the first line of the file
+	getline(file, newLine);
+	cout << newLine << endl;
+	
+	//TODO Check input for strings
+	if (newLine.length() < 5) {
+		cout << "The file is currupted." << endl;
+		cout << "The first line of the file is incorrect." << endl;
+		exit(3);
+	}
+
+	//Reading and initiating main class with number of boxes and size of the cont
+	getline(file, newLine, ' ');
+	container.width = stoi(newLine);
+	
+
+	getline(file, newLine, '\n');
+	container.length = stoi(newLine);
+	
+	getline(file, newLine);
+	container.numberOfBoxes = stoi(newLine);
+	
+	areaContainer = container.width * container.length;
 
 	
-	string firstLine;
-
-	getline(file, firstLine);
-	getline(file, firstLine);
-	PackerProblem problem = {(char) (*firstLine - "0"), 20, 20, 20};
-
+	//Reading and initiating box sizes and their names
 	while (file.good()) {
-		problem.boxes[0].length = 
+		getline(file, newLine, ' ');
+		container.boxes[counter].length = stoi(newLine);
+
+		getline(file, newLine, ' ');
+		container.boxes[counter].width = stoi(newLine);
+
+		getline(file, newLine, '\n');
+		container.boxes[counter].name = newLine[0];
+
+		//cout << (container.boxes[counter].length - '0')*(container.boxes[counter].width - '0') << endl;
+
+		sumAreaBoxes = sumAreaBoxes + (int)container.boxes[counter].length * (int)container.boxes[counter].width;
+		counter++;
 	}
+
+	if (sumAreaBoxes > areaContainer) {
+		cout << "The problem is unsolvable!" << endl;
+		cout << "The area of boxes exceed the area of the container." << endl;
+		cout << "Container area: " << areaContainer << " " << " Sum of the boxes' area: " << sumAreaBoxes << endl;
+		exit(3);
+	}
+
+	if (counter != container.numberOfBoxes) {
+		cout << "The file is currupted!" << endl;
+		cout << "Incorrect number of boxes provided/specified." << endl;
+		exit(3);
+	}
+
+
+	//TODO print container and boxes
+	return &container;
 }
 
 
 
 int main() {
-	PackerProblem container = {2000, 20};
 	
-	cout << (int)container.width;
 	
+	PackerProblem* pC = loadPackerProblem("input.txt");
+	
+
+	cout << sizeof(pC->numberOfBoxes);
 
 	
 }
